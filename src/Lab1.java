3231b5f50be438a8e7c41c66c6353b2fa30cbaa2
zjjim <zjjim@DESKTOP-HD9UDUD>
2019-09-03 15:01:44 -0700

    Removed tailing white space.

--- src/Lab1.java
+++ src/Lab1.java
@@ -7,7 +7,7 @@
 public class Lab1 {
 
 	public static void main(String[] args) {
-		// TODO Change this to "Hello CSC 335, I'm YOURNAME" 
+		// TODO Change this to "Hello CSC 335, I'm YOURNAME"
 		System.out.println("Hello CSC 335, I'm Zachary");
 	}
 
